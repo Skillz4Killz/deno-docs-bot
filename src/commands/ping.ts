29a44942bc124526b270a9ab0e5940a6df061204
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-05-06 18:05:17 -0400

    Initial commit

---
+++ src/commands/ping.ts
@@ -0,0 +1,23 @@
+// This file is meant to show how you can create multiple commands in the same file if you wish.
+import { Message } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/structures/message.ts"
+import { botCache } from "../../mod.ts"
+import { cache } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/utils/cache.ts"
+
+const pingCommand = (message: Message) => {
+  return message.channel.sendMessage(`Ping MS: ${Date.now() - message.timestamp}ms`)
+}
+
+const devPingCommand = (message: Message) => {
+  return message.channel.sendMessage(
+    `Ping MS: ${Date.now() - message.timestamp}ms | Guilds: ${cache.guilds.size} | Users: ${cache.users.size}`
+  )
+}
+
+botCache.commands.set(`ping`, {
+  callback: pingCommand,
+})
+
+botCache.commands.set(`dev_ping`, {
+  guildOnly: true,
+  callback: devPingCommand,
+})
---
+++ src/events/guildCreate.ts
@@ -0,0 +1,5 @@
+import { Guild } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/structures/guild.ts"
+
+export const guildCreate = (guild: Guild) => {
+  console.log(guild.name)
+}
